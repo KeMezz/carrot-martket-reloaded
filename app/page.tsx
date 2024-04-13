66fa54722dbd6f5e1b0dc603403883d45a5ee5cf
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-04-13 19:24:55 +0900

    animation and empty states

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 export default function Home() {
   return (
-    <main className="flex items-center justify-center h-screen p-5 bg-gray-300 sm:bg-red-400 md:bg-emerald-400 lg:bg-slate-400 xl:bg-pink-400">
-      <div className="flex flex-col w-full max-w-screen-sm gap-4 p-5 bg-white shadow-lg md:flex-row rounded-2xl">
-        <input
-          className="w-full h-12 px-5 py-2 transition-shadow bg-gray-200 rounded-full outline-none ring ring-transparent focus:ring-green-400 ring-offset-1 placeholder:drop-shadow-sm invalid:focus:ring-red-600 peer"
-          type="email"
-          required
-          placeholder="Email Address"
-        />
-        <span className="hidden font-medium text-red-500 peer-invalid:block">
-          Email is required
-        </span>
-        <button className="py-2 font-medium text-white transition-transform bg-black rounded-full outline-none active:scale-90 md:px-10 peer-invalid:bg-gray-300">
-          Log in
-        </button>
+    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
+        {["Nico", "Me", "You", "Yourself", ""].map((person, index) => (
+          <div key={index} className="flex items-center gap-5">
+            <div className="size-10 bg-blue-400 rounded-full" />
+            <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300">
+              {person}
+            </span>
+            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative">
+              <span className="z-10">{index}</span>
+              <div className="size-6 bg-red-500 rounded-full absolute animate-ping " />
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
