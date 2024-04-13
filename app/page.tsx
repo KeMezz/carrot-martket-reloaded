c01ce6544bd1626226004c0fed3011edface367f
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-04-14 01:39:37 +0900

    create root main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
-        <div className="group flex flex-col">
-          <input
-            className="bg-gray-100 w-full"
-            placeholder="Write your email"
-          />
-          <span className="group-focus-within:block hidden w-[104.392px]">
-            Make sure it is a valid email...
-          </span>
-          <button>Submit</button>
+    <div className="flex flex-col items-center justify-between min-h-screen p-5">
+      <div className="my-auto flex flex-col items-center gap-3">
+        <span className="text-9xl mb-4 animate-bounce">🥕</span>
+        <h1 className="text-4xl font-black">당근</h1>
+        <h2 className="text-2xl font-medium">만나서 반가워요! 👏</h2>
+      </div>
+      <div className="flex flex-col items-center gap-4 w-full">
+        <Link href="/create-account" className="primary-btn">
+          시작하기
+        </Link>
+        <div className="flex gap-2">
+          <span>이미 계정이 있나요?</span>
+          <Link href="/login" className="hover:underline">
+            로그인
+          </Link>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
