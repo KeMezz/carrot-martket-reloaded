52e25d4c4abca146c38dc496b2a25c694849ca92
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-04-14 20:05:25 +0900

    create validation with zod

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -2,12 +2,35 @@
 
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirm_password: z.string().min(10),
-});
+const checkUsername = (username: string) => !username.includes("potato");
+const checkPasswords = ({
+  password,
+  confirm_password,
+}: {
+  password: string;
+  confirm_password: string;
+}) => password === confirm_password;
+
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "유효하지 않은 형식입니다",
+        required_error: "이름은 필수 입력 사항이에요",
+      })
+      .min(3, "이름은 최소 3글자 이상이어야 해요")
+      .max(10, "이름은 최대 10글자 이하여야 해요")
+      .refine(checkUsername, "이름에 'potato'가 포함되어 있어요"),
+    email: z.string().email({ message: "올바른 형식의 이메일을 입력해주세요" }),
+    password: z.string().min(10, "비밀번호는 최소 10글자 이상이어야 해요"),
+    confirm_password: z
+      .string()
+      .min(10, "비밀번호는 최소 10글자 이상이어야 해요"),
+  })
+  .refine(checkPasswords, {
+    message: "비밀번호가 일치하지 않아요",
+    path: ["confirm_password"],
+  });
 
 export default async function createAccount(
   prevState: any,
