c09c4f038ad77fd59cde1fe325991cd9eee45fb3
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-04-15 22:51:49 +0900

    Update actions.ts

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,10 +1,6 @@
 "use server";
 
 export async function handleForm(prevState: any, formData: FormData) {
-  await new Promise((resolve) => setTimeout(resolve, 4000));
-  console.log(prevState);
-  console.log(formData.get("email"), formData.get("password"));
-  console.log("i run in the server!");
   return {
     errors: ["잘못된 계정 정보입니다.", "비밀번호가 너무 짧습니다."],
   };
