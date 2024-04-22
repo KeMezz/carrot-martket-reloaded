478a8658d48ceabc677f237337f3693b75cf2dba
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-04-22 23:53:11 +0900

    change name of function

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,3 +1,9 @@
 "use server";
 
-export async function smsVertification(prevState: any, formData: FormData) {}
+export async function smsLogin(prevState: any, formData: FormData) {
+  const data = {
+    phone_number: formData.get("phone_number"),
+  };
+  console.log(data);
+  return null;
+}
