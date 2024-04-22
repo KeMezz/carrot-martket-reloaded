b4dbe407eaaef1cb0668cad39da00505ff95e8aa
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-04-23 00:02:50 +0900

    create phone and token schema

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,5 +1,11 @@
 "use server";
 
+import { z } from "zod";
+import validator from "validator";
+
+const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const tokenSchema = z.coerce.number().min(100000).max(999999);
+
 export async function smsLogin(prevState: any, formData: FormData) {
   const data = {
     phone_number: formData.get("phone_number"),
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -2,11 +2,11 @@
 
 import Button from "@/components/button";
 import Input from "@/components/input";
-import { smsVertification } from "./actions";
+import { smsLogin } from "./actions";
 import { useFormState } from "react-dom";
 
 export default function SMSLogin() {
-  const [state, action] = useFormState(smsVertification, null);
+  const [state, action] = useFormState(smsLogin, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2">
@@ -18,7 +18,7 @@ export default function SMSLogin() {
       <form className="flex flex-col gap-5" action={action}>
         <Input
           name="phone"
-          type="number"
+          type="text"
           placeholder="휴대폰 번호"
           required={true}
         />
@@ -27,6 +27,8 @@ export default function SMSLogin() {
           type="number"
           placeholder="인증 코드"
           required={true}
+          min={100000}
+          max={999999}
         />
         <Button text="인증하기" />
       </form>
