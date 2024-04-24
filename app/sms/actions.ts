c36e4a016f8a66e7d2c4c866a56a764bf1a7fe49
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-04-24 23:09:49 +0900

    add region infomation

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -3,12 +3,16 @@
 import { z } from "zod";
 import validator from "validator";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine((phone) => validator.isMobilePhone(phone, "ko-KR"));
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
 export async function smsLogin(prevState: any, formData: FormData) {
   const data = {
     phone_number: formData.get("phone_number"),
+    token: formData.get("token"),
   };
   console.log(data);
   return null;
