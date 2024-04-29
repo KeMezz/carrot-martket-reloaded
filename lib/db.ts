e38885879eac0390273b55657f7ba178685f324a
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-04-29 14:25:51 +0900

    create test prisma sql

--- lib/db.ts
+++ lib/db.ts
@@ -2,4 +2,17 @@ import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
+async function test() {
+  const users = await db.user.findMany({
+    where: {
+      username: {
+        contains: "est",
+      },
+    },
+  });
+  console.log(users);
+}
+
+test();
+
 export default db;
