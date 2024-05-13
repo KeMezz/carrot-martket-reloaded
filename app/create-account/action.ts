ddda9ec31b6472190696c97aed8682ebbdc2528b
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-05-13 23:46:01 +0900

    check username and email using zod async

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -1,11 +1,15 @@
 "use server";
 
 import {
+  EMAIL_ALREADY_EXISTS_MESSAGE,
   EMAIL_ERROR_MESSAGE,
+  INVALID_TYPE_ERROR_MESSAGE,
   PASSWORD_MIN_ERROR_MESSAGE,
   PASSWORD_MIN_LENGTH,
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR_MESSAGE,
+  REQUIRED_ERROR_MESSAGE,
+  USERNAME_ALREADY_EXISTS_MESSAGE,
   USERNAME_MAX_ERROR_MESSAGE,
   USERNAME_MAX_LENGTH,
   USERNAME_MIN_ERROR_MESSAGE,
@@ -14,7 +18,6 @@ import {
 import db from "@/lib/db";
 import { z } from "zod";
 
-const checkUsername = (username: string) => !username.includes("potato");
 const checkPasswords = ({
   password,
   confirm_password,
@@ -23,25 +26,52 @@ const checkPasswords = ({
   confirm_password: string;
 }) => password === confirm_password;
 
+const checkUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username: username,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !!user;
+};
+
+const checkUserEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email: email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !!user;
+};
+
 const formSchema = z
   .object({
     username: z
       .string({
-        invalid_type_error: "유효하지 않은 형식입니다",
-        required_error: "이름은 필수 입력 사항이에요",
+        invalid_type_error: INVALID_TYPE_ERROR_MESSAGE,
+        required_error: REQUIRED_ERROR_MESSAGE,
       })
       .min(USERNAME_MIN_LENGTH, USERNAME_MIN_ERROR_MESSAGE)
       .max(USERNAME_MAX_LENGTH, USERNAME_MAX_ERROR_MESSAGE)
       .toLowerCase()
       .trim()
-      .refine(checkUsername, "이름에 'potato'가 포함되어 있어요"),
-    email: z.string().email({ message: EMAIL_ERROR_MESSAGE }).toLowerCase(),
+      .refine(checkUsername, USERNAME_ALREADY_EXISTS_MESSAGE),
+    email: z
+      .string()
+      .email({ message: EMAIL_ERROR_MESSAGE })
+      .toLowerCase()
+      .refine(checkUserEmail, EMAIL_ALREADY_EXISTS_MESSAGE),
     password: z.string(),
     // .min(PASSWORD_MIN_LENGTH, PASSWORD_MIN_ERROR_MESSAGE)
     // .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR_MESSAGE),
-    confirm_password: z
-      .string()
-      .min(PASSWORD_MIN_LENGTH, PASSWORD_MIN_ERROR_MESSAGE),
+    confirm_password: z.string(),
+    // .min(PASSWORD_MIN_LENGTH, PASSWORD_MIN_ERROR_MESSAGE),
   })
   .refine(checkPasswords, {
     message: "비밀번호가 일치하지 않아요",
@@ -61,42 +91,13 @@ export default async function createAccount(
 
   // By using safeParse, we can get the errors in a flattened format
   // if we not use safeParse, we have to use try-catch block to get the errors
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
   if (!result.success) {
     return { errors: result.error.flatten() };
   } else {
-    // 유저명이 이미 존재하는지 확인한다
-    const user = await db.user.findUnique({
-      where: {
-        username: result.data.username,
-      },
-      select: {
-        id: true,
-      },
-    });
-    if (user) {
-      // 에러 메시지를 보여준다
-    }
-
-    // 이메일이 이미 존재하는지 확인한다
-    const userEmail = await db.user.findUnique({
-      where: {
-        email: result.data.email,
-      },
-      select: {
-        id: true,
-      },
-    });
-    if (userEmail) {
-      // 에러 메시지를 보여준다
-    }
-
-    // 패스워드를 해싱한패
-
-    // 데이터베이스에 유저를 추가한터
-
+    // 패스워드를 해싱한다
+    // 데이터베이스에 유저를 추가한다
     // 유저를 로그인 시킨다
-
     // 유저를 /home 페이지로 리다이렉트 시킨다
   }
 }
