51a56f8e0b93a42d392b860041fb7c442e642764
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-05-22 19:47:29 +0900

    implements logout function

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,37 @@
-export default function Profile() {
-  return <h1>welcome to your profile</h1>;
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { redirect } from "next/navigation";
+
+async function getUser() {
+  const session = await getSession();
+  if (session.id) {
+    const user = await db.user.findUnique({
+      where: {
+        id: session.id,
+      },
+      select: {
+        id: true,
+        username: true,
+      },
+    });
+    return user;
+  }
+}
+
+export default async function Profile() {
+  const user = await getUser();
+  const logOut = async () => {
+    "use server";
+    const session = await getSession();
+    session.destroy();
+    redirect("/");
+  };
+  return (
+    <div>
+      <h1>Welcome! {user?.username}</h1>
+      <form action={logOut}>
+        <button className="border p-2 rounded-lg">Log Out</button>
+      </form>
+    </div>
+  );
 }
