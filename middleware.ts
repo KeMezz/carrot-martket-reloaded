0bd1951a8a7dcff2ce1daeaa092f04fc29c5a18c
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-05-25 18:56:19 +0900

    authentication middleware

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import getSession from "./lib/session";
 
+interface Routes {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+};
+
 export async function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname;
-  if (pathname === "/") {
-    const response = NextResponse.next();
-    response.cookies.set("middleware-cookies", "hello!");
-    return response;
-  }
-  if (pathname === "/profile") {
-    return Response.redirect(new URL("/", request.url));
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
+  if (!session.id) {
+    if (!exists) {
+      // 유저가 로그인 하지 않은 상태에서 로그인 유저만 접근 가능한 url에 접속했을 때
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    // 이미 로그인 한 유저가 비로그인 유저만 접근할 수 있는 url에 접근했을 때
+    return NextResponse.redirect(new URL("/products", request.url));
   }
 }
 
