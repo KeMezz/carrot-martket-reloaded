f6219436a1c11d7aff52b445228afdb15526b520
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-06-03 22:36:05 +0900

    add todos

--- app/github/complete/route.ts
+++ app/github/complete/route.ts
@@ -3,6 +3,7 @@ import getSession from "@/lib/session";
 import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
 
+// TODO: request와 response 부분을 별도의 함수로 분리하기
 export async function GET(request: NextRequest) {
   const code = request.nextUrl.searchParams.get("code");
   if (!code) {
@@ -24,14 +25,17 @@ export async function GET(request: NextRequest) {
   });
   const { error, access_token } = await accessTokenResponse.json();
   if (error) {
+    // TODO: /github/error 같은 페이지로 리다이렉트
     return new Response(null, {
       status: 400,
     });
   }
+  // TODO: user/emails로 새로운 요청을 보내서 이메일을 가져옵니다.
   const userProfileResponse = await fetch("https://api.github.com/user", {
     headers: {
       Authorization: `Bearer ${access_token}`,
     },
+    // 해당 리퀘스트는 이를 요청하는 모든 유저마다 다르게 기록되어야 하므로, 캐싱을 하지 않습니다.
     cache: "no-cache",
   });
   const { id, avatar_url, login } = await userProfileResponse.json();
@@ -44,11 +48,13 @@ export async function GET(request: NextRequest) {
     },
   });
   if (user) {
+    // TODO: 로그인 처리를 별도의 함수로 관리하기
     const session = await getSession();
     session.id = user.id;
     await session.save();
     return redirect("/profile");
   }
+  // TODO: 깃허브 로그인 처리 전에 이미 존재하는 유저명이 있는지 확인하기
   const newUser = await db.user.create({
     data: {
       username: login,
