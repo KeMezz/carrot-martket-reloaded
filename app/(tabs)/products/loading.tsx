cc10c68e7e693227e9ae8de8288576eea6b67681
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-06-24 20:00:32 +0900

    edit loading page

--- app/(tabs)/products/loading.tsx
+++ app/(tabs)/products/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
       {[...Array(10)].map((_, index) => (
         <div className="flex gap-5" key={index}>
           <div className="size-28 bg-neutral-700 rounded-md" />
-          <div className="flex flex-col gap-2 *:rounded-md">
+          <div className="flex flex-col gap-2 *:rounded-md justify-center">
             <div className="bg-neutral-700 h-5 w-40" />
             <div className="bg-neutral-700 h-5 w-20" />
             <div className="bg-neutral-700 h-5 w-10" />
