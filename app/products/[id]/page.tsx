c47c7f1cf13801a7f74d44fb13c0ea35cd880ea5
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-06-25 19:26:47 +0900

    design product detail page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,5 +1,9 @@
 import db from "@/lib/db";
 import getSession from "@/lib/session";
+import { formatToWon } from "@/lib/utils";
+import { UserIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 async function getIsOwner(userId: number) {
@@ -42,5 +46,51 @@ export default async function ProductDetail({
   }
   const isOwner = await getIsOwner(product.userId);
 
-  return <span>Product Detail of the {id}</span>;
+  return (
+    <div>
+      <div className="relative aspect-square">
+        <Image fill src={product.photo} alt={product.title} />
+      </div>
+      <div className="p-5 flex items-center gap-3 border-b border-neutral-700">
+        <div className="size-10 rounded-full">
+          {product.user.avatar !== null ? (
+            <Image
+              className="rounded-full"
+              src={product.user.avatar}
+              width={40}
+              height={40}
+              alt={product.user.username}
+            />
+          ) : (
+            <UserIcon />
+          )}
+        </div>
+        <div>
+          <h3>{product.user.username}</h3>
+        </div>
+      </div>
+      <div className="p-5">
+        <h1 className="text-2xl font-semibold">{product.title}</h1>
+        <p>{product.description}</p>
+      </div>
+      <div className="fixed w-full bottom-0 left-0 p-5 pb-10 bg-neutral-800 flex justify-between items-center">
+        <span className="font-semibold text-xl">
+          {formatToWon(product.price)}원
+        </span>
+        <div className="flex gap-4">
+          {isOwner ? (
+            <button className="bg-red-500 px-5 py-2.5 rounded-md text-white font-semibold">
+              삭제하기
+            </button>
+          ) : null}
+          <Link
+            className="bg-orange-500 px-5 py-2.5 rounded-md text-white font-semibold"
+            href={``}
+          >
+            채팅하기
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
