25088db414462248ec55ed50b5ef31741806a5a9
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-07-10 22:02:29 +0900

    upload new product

--- app/products/add/actions.ts
+++ app/products/add/actions.ts
@@ -1,6 +1,29 @@
 "use server";
 
-export async function uploadProduct(formData: FormData) {
+import { z } from "zod";
+import fs from "fs/promises";
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { redirect } from "next/navigation";
+
+const productSchema = z.object({
+  photo: z
+    .string({
+      required_error: "사진을 추가해주세요.",
+    })
+    .max(255, "파일 이름이 너무 깁니다. 255자 이하로 입력해주세요."),
+  title: z.string({
+    required_error: "상품명을 입력해주세요.",
+  }),
+  price: z.coerce.number({
+    required_error: "가격을 입력해주세요.",
+  }),
+  description: z.string({
+    required_error: "상품 설명을 입력해주세요.",
+  }),
+});
+
+export async function uploadProduct(_: any, formData: FormData) {
   const data = {
     photo: formData.get("photo"),
     title: formData.get("title"),
@@ -8,5 +31,36 @@ export async function uploadProduct(formData: FormData) {
     description: formData.get("description"),
   };
 
-  console.log(data);
+  if (data.photo instanceof File) {
+    const photoData = await data.photo.arrayBuffer();
+    await fs.appendFile(`./public/${data.photo.name}`, Buffer.from(photoData));
+    data.photo = `/${data.photo.name}`;
+  }
+
+  const result = productSchema.safeParse(data);
+  if (!result.success) {
+    console.log(result.error.flatten());
+    return result.error.flatten();
+  } else {
+    const session = await getSession();
+    if (session.id) {
+      const product = await db.product.create({
+        data: {
+          title: result.data.title,
+          description: result.data.description,
+          price: result.data.price,
+          photo: result.data.photo,
+          user: {
+            connect: {
+              id: session.id,
+            },
+          },
+        },
+        select: {
+          id: true,
+        },
+      });
+      redirect(`/products/${product.id}`);
+    }
+  }
 }
--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -5,9 +5,13 @@ import Input from "@/components/input";
 import { PhotoIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 import { uploadProduct } from "./actions";
+import { useFormState } from "react-dom";
+import { ExclamationTriangleIcon } from "@heroicons/react/16/solid";
 
 export default function AddProduct() {
   const [preview, setPreview] = useState("");
+  const [state, action] = useFormState(uploadProduct, null);
+
   const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const {
       target: { files },
@@ -15,12 +19,17 @@ export default function AddProduct() {
     if (!files) return;
 
     const file = files[0];
+    if (file.size > 1024 * 1024 * 5) {
+      alert("5MB 이하의 파일만 업로드 가능합니다.");
+
+      return;
+    }
     const url = URL.createObjectURL(file);
     setPreview(url);
   };
   return (
     <div>
-      <form action={uploadProduct} className="p-5 flex flex-col gap-5">
+      <form action={action} className="p-5 flex flex-col gap-5">
         <label
           htmlFor="photo"
           className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer hover:border-orange-500 *:hover:text-orange-400 transition-colors bg-center bg-cover"
@@ -35,6 +44,12 @@ export default function AddProduct() {
             </>
           ) : null}
         </label>
+        {state?.fieldErrors.photo ? (
+          <span className="text-red-500 font-medium flex items-center gap-2 text-sm">
+            <ExclamationTriangleIcon className="size-4 mt-[0.5px]" />
+            {state?.fieldErrors.photo}
+          </span>
+        ) : null}
         <input
           onChange={onImageChange}
           type="file"
@@ -43,13 +58,26 @@ export default function AddProduct() {
           accept="image/*"
           className="hidden"
         />
-        <Input name="title" required placeholder="상품명" type="text" />
-        <Input name="price" type="number" required placeholder="가격" />
+        <Input
+          name="title"
+          required
+          placeholder="상품명"
+          type="text"
+          errors={state?.fieldErrors.title}
+        />
+        <Input
+          name="price"
+          type="number"
+          required
+          placeholder="가격"
+          errors={state?.fieldErrors.price}
+        />
         <Input
           name="description"
           type="text"
           required
           placeholder="자세한 설명"
+          errors={state?.fieldErrors.description}
         />
         <Button text="작성 완료" />
       </form>
