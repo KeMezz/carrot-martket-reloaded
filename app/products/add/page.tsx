b24b315657ac495dd05a4f7188b150ea38a3cb5b
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-07-10 21:22:05 +0900

    preview the user uploaded image

--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -1,26 +1,48 @@
+"use client";
+
 import Button from "@/components/button";
 import Input from "@/components/input";
 import { PhotoIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
 
 export default function AddProduct() {
+  const [preview, setPreview] = useState("");
+  const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { files },
+    } = event;
+    if (!files) return;
+
+    const file = files[0];
+    const url = URL.createObjectURL(file);
+    setPreview(url);
+  };
   return (
     <div>
       <form className="p-5 flex flex-col gap-5">
         <label
           htmlFor="photo"
-          className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer hover:border-orange-500 hover:text-orange-500 transition-colors"
+          className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer hover:border-orange-500 *:hover:text-orange-400 transition-colors bg-center bg-cover"
+          style={{ backgroundImage: `url(${preview})` }}
         >
-          <PhotoIcon className="w-20" />
-          <div className="text-neutral-400 text-sm">사진을 추가해주세요.</div>
+          {preview === "" ? (
+            <>
+              <PhotoIcon className="w-20" />
+              <div className="text-neutral-400 text-sm">
+                사진을 추가해주세요.
+              </div>
+            </>
+          ) : null}
         </label>
         <input
+          onChange={onImageChange}
           type="file"
           id="photo"
           name="photo"
           accept="image/*"
           className="hidden"
         />
-        <Input name="title" required placeholder="제목" type="text" />
+        <Input name="title" required placeholder="상품명" type="text" />
         <Input name="price" type="number" required placeholder="가격" />
         <Input
           name="description"
