ea57d73ce3ff92dbf0be408c5a3999227b382b8b
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-08-27 18:21:44 +0900

    add revalidate timer

--- app/(tabs)/home/page.tsx
+++ app/(tabs)/home/page.tsx
@@ -35,7 +35,7 @@ export const metadata = {
   title: "Home",
 };
 
-export const dynamic = "force-dynamic";
+export const revalidate = 60;
 
 export default async function Products() {
   const initialProducts = await getInitialProducts();
