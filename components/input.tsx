da4285bb97e437ad5816911fed9b5ea469d23120
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-09-10 22:40:02 +0900

    add register prop

--- components/input.tsx
+++ components/input.tsx
@@ -1,16 +1,19 @@
 import { ExclamationTriangleIcon } from "@heroicons/react/16/solid";
 import { InputHTMLAttributes, RefObject } from "react";
+import { UseFormRegisterReturn } from "react-hook-form";
 
 interface InputProps {
   name: string;
   errors?: string[];
   inputRef?: RefObject<HTMLInputElement>;
+  register?: UseFormRegisterReturn;
 }
 
 export default function Input({
   name,
   errors = [],
   inputRef,
+  register,
   ...attrs
 }: InputProps & InputHTMLAttributes<HTMLInputElement>) {
   return (
@@ -20,6 +23,7 @@ export default function Input({
         className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-500"
         name={name}
         ref={inputRef}
+        {...register}
         {...attrs}
       />
       {errors?.length ? (
