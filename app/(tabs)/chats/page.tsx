a309290a312588e5cd49d054636b78ecb06a0be3
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-10-30 20:21:02 +0900

    채팅 목록 페이지 작성

--- app/(tabs)/chats/page.tsx
+++ app/(tabs)/chats/page.tsx
@@ -1,7 +1,73 @@
-export default function Chat() {
+// TODO: 채팅방의 읽음 여부를 확인하고 표시하기
+// TODO: 최신 메시지가 변경되면 캐시로 채팅 목록까지 갱신시키기
+
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import Image from "next/image";
+import Link from "next/link";
+
+async function getChatRooms() {
+  const session = await getSession();
+  const chatRooms = await db.chatRoom.findMany({
+    where: {
+      users: {
+        some: {
+          id: session.id!,
+        },
+      },
+    },
+    include: {
+      messages: {
+        select: {
+          text: true,
+        },
+        take: 1,
+        orderBy: {
+          created_at: "desc",
+        },
+      },
+      users: {
+        where: {
+          id: {
+            not: session.id,
+          },
+        },
+        select: {
+          avatar: true,
+          username: true,
+        },
+      },
+    },
+  });
+
+  return chatRooms;
+}
+
+export default async function Chat() {
+  const chatRooms = await getChatRooms();
   return (
-    <div>
-      <h1 className="text-white text-4xl">Chat!</h1>
+    <div className="p-2 divide-y">
+      {chatRooms.map((chatRoom) => (
+        <Link
+          href={`/chats/${chatRoom.id}`}
+          key={chatRoom.id}
+          className="flex gap-4 p-4 items-center text-white"
+        >
+          {chatRoom.users[0].avatar ? (
+            <Image
+              src={chatRoom.users[0].avatar}
+              width={40}
+              height={40}
+              alt={chatRoom.users[0].username}
+              className="size-14 rounded-full"
+            />
+          ) : null}
+          <div>
+            <h3 className="font-bold">{chatRoom.users[0].username}</h3>
+            <p>{chatRoom.messages[0].text}</p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 }
