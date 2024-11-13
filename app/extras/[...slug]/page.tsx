92f2754637f00f6a9258a413d40eea77b7b4630a
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-11-13 21:43:21 +0900

    optional catch all parameter

--- app/extras/[...slug]/page.tsx
+++ app/extras/[...slug]/page.tsx
@@ -1,4 +1,5 @@
-export default function Extra() {
+export default function Extra({ params }: { params: { slug: string[] } }) {
+  console.log(params.slug);
   return (
     <div className="p-4">
       <h1 className="font-bold text-4xl font-rubik">Extras!</h1>
