2c71c1f96cc6a124f756ff77ef0163e752730bd0
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-11-18 20:54:49 +0900

    extra: images

--- app/extras/[[...slug]]/page.tsx
+++ app/extras/[[...slug]]/page.tsx
@@ -1,26 +1,28 @@
 import HackedComponent from "@/components/hacked-component";
+import Image from "next/image";
 import {
   experimental_taintObjectReference,
   experimental_taintUniqueValue,
 } from "react";
+import heavyImage from "@/public/DSCF8565.jpeg";
 
 async function getData() {
   const keys = {
     apiKey: "124124",
     secret: "0ewoijfaowe",
   };
-  // experimental_taintObjectReference("API Keys were leaked!", keys);
-  // experimental_taintUniqueValue("Secret key was exposed", keys, keys.secret);
+  experimental_taintObjectReference("API Keys were leaked!", keys);
+  experimental_taintUniqueValue("Secret key was exposed", keys, keys.secret);
 
   return keys;
 }
 
 export default async function Extra() {
-  const data = await getData();
   return (
     <div className="p-4">
-      <h1 className="font-bold text-4xl font-rubik">Extras!</h1>
-      <HackedComponent data={data} />
+      <h1 className="text-4xl font-bold font-rubik">Extras!</h1>
+      <Image src={heavyImage} alt="profile" placeholder="blur" />
+      {/* <HackedComponent data={data} /> */}
     </div>
   );
 }
--- components/hacked-component.tsx
+++ components/hacked-component.tsx
@@ -3,7 +3,5 @@
 import { fetchFromAPI } from "@/app/extras/action";
 
 export default function HackedComponent({ data }: { data: any }) {
-  fetchFromAPI();
-
   return <div>hacked!</div>;
 }
